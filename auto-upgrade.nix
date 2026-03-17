7ecd90b356d47a28c4d40f0c9a677d5e453d682a
Aidan Wright <38870143+AidanWright@users.noreply.github.com>
2026-03-17 01:17:32 -0400

    Updated auto-upgrade flags
    Shouldn't have listened to claude smh
    - removed `--commit-lock-file` as not necessary when tracking via remote   git

--- auto-upgrade.nix
+++ auto-upgrade.nix
@@ -4,7 +4,6 @@
     flake = "github:UConn-Skydiving/nix#netcup";
     flags = [
       "--print-build-logs"
-      "--commit-lock-file"  # If you want to automatically commit the updated flake.lock
     ];
     dates = "02:00";
     randomizedDelaySec = "45min";
