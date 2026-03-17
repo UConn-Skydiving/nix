f6d8821cb39840d2f384a1fcca4abe7c6d9a8ab4
Aidan Wright <38870143+AidanWright@users.noreply.github.com>
2026-03-17 00:01:40 -0400

    fix flake.nix flake directory to point to this repo

--- auto-upgrade.nix
+++ auto-upgrade.nix
@@ -1,12 +1,13 @@
 {
   system.autoUpgrade = {
     enable = true;
-    flake = "/path/to/flake";
+    flake = "github:UConn-Skydiving/nix#netcup";
     flags = [
       "--print-build-logs"
       "--commit-lock-file"  # If you want to automatically commit the updated flake.lock
     ];
     dates = "02:00";
     randomizedDelaySec = "45min";
+    allowReboot = true;
   };
 }
